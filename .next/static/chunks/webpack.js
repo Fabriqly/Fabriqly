3047fa59c58d0585617bca4b3bc8f817b21bbedb
Marc <mmarclawrence@gmail.com>
2025-09-03 22:10:03 +0800

    updated the readme.md

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f149454d855d77fb"; }
+/******/ 		__webpack_require__.h = function() { return "95907f77bf14cbc9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
